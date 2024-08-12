aa06754a5a77a613e20b3e5684dcfecbf48f8102
Roman Trukhanovich <trukroman@gmail.com>
2024-08-12 11:18:44 +0300

    In long text, given two words, find the closest distance between them

---
+++ interview/ClosestDistance.java
@@ -0,0 +1,44 @@
+package interview;
+
+public class ClosestDistance {
+    public static void main(String[] args) {
+        String text = "The Paris Olympics opened with rain on its parade, then blistering "
+                      + "heat and, finally, a week of pleasant sunshine.";
+        String firstWord = "Paris";
+        String secondWord = "with";
+
+        int distant = findClosestDistant(text, firstWord, secondWord);
+        System.out.println("The closest distance is " + distant);
+    }
+
+    private static int findClosestDistant(String text,
+                                          String firstWord,
+                                          String secondWord) {
+        String[] splitText = text.split("\s");
+        int firstWordIndex = -1;
+        int secondWordIndex = -1;
+        int minDistance = Integer.MAX_VALUE;
+
+        for (int i = 0; i < splitText.length; i++) {
+            if (splitText[i].equals(firstWord)) {
+                firstWordIndex = i;
+            }
+            if (splitText[i].equals(secondWord)) {
+                secondWordIndex = i;
+            }
+
+            if (firstWordIndex != -1 && secondWordIndex != -1) {
+                int currentDistance = Math.abs(secondWordIndex - firstWordIndex);
+                if (currentDistance < minDistance) {
+                    minDistance = currentDistance;
+                }
+            }
+        }
+
+        if (minDistance == Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("Word not found");
+        }
+
+        return minDistance;
+    }
+}
