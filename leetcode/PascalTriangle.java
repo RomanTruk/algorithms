d651612086cd7eafdfdd6dc42834882375b0d753
Roman Trukhanovich <roman.trukhanovich@gmail.com>
2024-09-04 12:40:22 +0300

    118. Pascal's Triangle https://leetcode.com/problems/pascals-triangle

---
+++ leetcode/PascalTriangle.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+        System.out.println(generate(5));
+    }
+
+    private static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>(new ArrayList<>());
+
+        List<Integer> firstRow = List.of(1);
+        result.add(firstRow);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = result.get(i - 1);
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+
+            for (int j = 1; j < i; j++) {
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+
+            row.add(1);
+            result.add(row);
+        }
+
+        return result;
+    }
+
+}
