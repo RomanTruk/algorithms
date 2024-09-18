c1a5ebd686228ae0d50edfe52010c03a62169146
Roman Trukhanovich <roman.trukhanovich@gmail.com>
2024-09-18 14:25:44 +0300

    628. Maximum Product of Three Numbers https://leetcode.com/problems/maximum-product-of-three-numbers/description/

---
+++ leetcode/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+public class MaximumProductOfThreeNumbers {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(maximumProduct(nums));
+    }
+
+    private static int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (max1 <= num) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (max2 <= num) {
+                max3 = max2;
+                max2 = num;
+            } else if (max3 <= num) {
+                max3 = num;
+            }
+
+            if (min1 >= num) {
+                min2 = min1;
+                min1 = num;
+            } else if (min2 >= num) {
+                min2 = num;
+            }
+        }
+
+        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
+    }
+}
