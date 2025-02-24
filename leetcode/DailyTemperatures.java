cbbc6b56ab159e73702d6a2c52e1fa9b6da505c5
Roman Trukhanovich <roman.trukhanovich@gmail.com>
2025-02-24 17:49:25 +0300

    739. Daily Temperatures https://leetcode.com/problems/daily-temperatures

---
+++ leetcode/DailyTemperatures.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class DailyTemperatures {
+    public static void main(String[] args) {
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+        int[] result = dailyTemperatures(temperatures);
+
+        for (int r : result) {
+            System.out.print(r + " ");
+        }
+    }
+
+    private static int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                result[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+
+}
