8d754abc7f1581cfe542698a301ef79ce51106f9
Roman Trukhanovich <roman.trukhanovich@gmail.com>
2025-04-17 12:34:11 +0300

    2255. Count Prefixes of a Given String https://leetcode.com/problems/count-prefixes-of-a-given-string

---
+++ leetcode/CountPrefixesOfGivenString.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class CountPrefixesOfGivenString {
+    public int countPrefixes(String[] words, String s) {
+        int count = 0;
+        for (String word : words) {
+            if (s.startsWith(word)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
