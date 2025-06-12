d1b0f8aa29c98c3a71247449df75aeb8993ddb36
Roman Trukhanovich <roman.trukhanovich@gmail.com>
2025-06-12 15:52:38 +0300

    459. Repeated Substring Pattern https://leetcode.com/problems/repeated-substring-pattern

---
+++ leetcode/RepeatedSubstringPattern.java
@@ -0,0 +1,8 @@
+package leetcode;
+
+public class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        String str = s + s;
+        return str.substring(1, str.length() - 1).contains(s);
+    }
+}
